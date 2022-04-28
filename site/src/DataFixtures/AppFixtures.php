e56a64e5040cdf1e938a25662c7eb8472f6fc7d4
Tigerwang79 <valdrinsalihi@hotmail.fr>
2022-04-28 16:51:50 +0200

    App Fixtures

--- site/src/DataFixtures/AppFixtures.php
+++ site/src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Produit;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,9 +10,13 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
+        for ($i = 0; $i < 50; $i++) {
+            $produit = new Produit();
+            $produit->setLibelle('Carte Graphique n°' . $i);
+            $produit->setPrix(mt_rand(150, 3500));
+            $produit->setQuantite(mt_rand(0, 1000));
+            $manager->persist($produit);
+        }
         $manager->flush();
     }
 }
